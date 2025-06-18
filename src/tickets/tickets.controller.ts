45dfe8d618fd810847087d1f9cf165ecffcc759a
Tín Phạm <tinpham@Tins-MacBook-Pro.local>
2025-06-18 19:34:37 +0700

    feat:1 change requests, improve performance and stablility

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,4 +1,5 @@
-import { Body, ConflictException, Controller, Get, Post } from '@nestjs/common';
+import { Body, ConflictException,BadRequestException, Controller, Get, Post } from '@nestjs/common';
+import { Op } from 'sequelize';
 import { Company } from '../../db/models/Company';
 import {
   Ticket,
@@ -33,38 +34,97 @@ export class TicketsController {
   async create(@Body() newTicketDto: newTicketDto) {
     const { type, companyId } = newTicketDto;
 
-    const category =
-      type === TicketType.managementReport
-        ? TicketCategory.accounting
-        : TicketCategory.corporate;
+    if (!Object.values(TicketType).includes(type)) {
+      throw new BadRequestException("Invalid ticket type");
+    }
 
-    const userRole =
-      type === TicketType.managementReport
-        ? UserRole.accountant
-        : UserRole.corporateSecretary;
+    if (type === TicketType.managementReport) {
+      return this.createTicketManagementReport(companyId);
+    }
+
+    if (type === TicketType.registrationAddressChange) {
+      return this.createTicketAddressChange(companyId);
+    }
+
+    throw new BadRequestException("Unsupported ticket type");
+  }
+
+  private async createTicketManagementReport(companyId: number) {
+    const ticketType = TicketType.managementReport;
+    const category = TicketCategory.accounting;
+    const userRole = UserRole.accountant;
 
     const assignees = await User.findAll({
       where: { companyId, role: userRole },
       order: [['createdAt', 'DESC']],
     });
 
-    if (!assignees.length)
-      throw new ConflictException(
-        `Cannot find user with role ${userRole} to create a ticket`,
-      );
-
-    if (userRole === UserRole.corporateSecretary && assignees.length > 1)
-      throw new ConflictException(
-        `Multiple users with role ${userRole}. Cannot create a ticket`,
-      );
+    if (!assignees.length) 
+      throw new ConflictException("Cannot find accountant to create a ticket");
 
     const assignee = assignees[0];
 
+    return (await this.createTicket(assignee.id, companyId, category, ticketType));
+  }
+
+  private async createTicketAddressChange(companyId: number) {
+    const ticketType = TicketType.registrationAddressChange;
+    const category = TicketCategory.corporate;
+    const userRoles = [UserRole.corporateSecretary, UserRole.director];
+
+    const [existingTicket, assignees] = await Promise.all([
+      Ticket.findOne({ 
+        where: {
+          companyId,
+          type: ticketType,
+        },
+      }),
+      User.findAll({
+        where: { 
+          companyId, 
+          role: { [Op.in]: userRoles } 
+        },
+        order: [['createdAt', 'DESC']],
+      }),
+    ])
+    if (existingTicket) 
+      throw new ConflictException("Ticket already exists");
+
+    if (!assignees.length) 
+      throw new ConflictException("Cannot find secretary or director to create a ticket");
+
+    let secretaryCount = 0;
+    let directorCount = 0;
+    let secretary: User | null = null;
+    let director: User | null = null;
+
+    for (const assignee of assignees) {
+      if (assignee.role === UserRole.corporateSecretary) {
+        secretaryCount++;
+        secretary = assignee;
+      }
+      if (assignee.role === UserRole.director) {
+        directorCount++;
+        director = assignee;
+      }
+    }
+    if (secretaryCount > 1) 
+      throw new ConflictException("Multiple users with role Corporate Secretary. Cannot create a ticket");
+    
+    if (directorCount > 1) 
+      throw new ConflictException("Multiple users with role Director. Cannot create a ticket");
+    
+    const assignee = (secretary || director) as User;
+
+    return (await this.createTicket(assignee.id, companyId, category, ticketType));
+  }
+
+  private async createTicket(assigneeId: number, companyId: number, category: TicketCategory, ticketType: TicketType) {
     const ticket = await Ticket.create({
+      assigneeId,
       companyId,
-      assigneeId: assignee.id,
       category,
-      type,
+      type: ticketType,
       status: TicketStatus.open,
     });
 
